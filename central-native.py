65fbef1d4d9005c35f75e31e3aad8d0b33c1214b
emann0525 <84114845+emann0525@users.noreply.github.com>
2021-05-19 21:55:15 -0500

    Update central-native.py

--- central-native.py
+++ central-native.py
@@ -1,1 +1,22 @@
+In [1]: values = (47, 95, 88, 73, 88, 84)
 
+In [2]: len(values)
+Out[2]: 6
+
+In [3]: sum(values)
+Out[3]: 475
+
+In [4]: sum(values)/len(values)
+Out[4]: 79.16666666666667
+
+In [5]: import numpy
+
+In [6]: x = numpy.median(values)
+
+In [7]: print (x)
+86.0
+
+In [8]: sorted(values)
+Out[8]: [47, 73, 84, 88, 88, 95]
+
+In [9]: 88 is the mode
