7903511cf308a0910e0fa8253994bb5cfa277364
emann0525 <84114845+emann0525@users.noreply.github.com>
2021-05-19 21:58:09 -0500

    Update central-with-statistics.py

--- central-with-statistics.py
+++ central-with-statistics.py
@@ -1,1 +1,18 @@
+In [1]: import statistics
 
+In [2]: values = (47, 95, 88, 73, 88, 84)
+
+In [3]: sum(values)
+Out[3]: 475
+
+In [4]: len(values)
+Out[4]: 6
+
+In [5]: statistics.mean(values)
+Out[5]: 79.16666666666667
+
+In [6]: statistics.median(values)
+Out[6]: 86.0
+
+In [7]: statistics.mode(values)
+Out[7]: 88
